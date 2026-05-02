be02b2fda720bcfb014b9ed2e8b6fb163da20c77
Hayden Bourgeois <haydenbourgeois04@gmail.com>
2026-05-01 23:32:52 -0400

    Finalized code

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
 
 print('===== STARTING TRAINING AND VALIDATING =====')
 # LOAD TRAINING AND VALIDATION DATA
@@ -33,7 +38,7 @@ X_val = valDF[featureColumns]
 y_val = valDF['labelA']
 
 # INITALIZE MODEL
-model = LogisticRegression()
+model = RandomForestClassifier(n_estimators=100, class_weight='balanced', random_state=0)
 
 # TRAIN THE MODEL
 print("Training the model...")
@@ -74,3 +79,32 @@ print(f"Final Test Accuracy: {test_accuracy * 100:.2f}%\n")
 print("Final Test Classification Report:")
 print(classification_report(y_test, test_predictions, target_names=['Real (0)', 'AI (1)']))
 print('===== FINISHED TESTING =====')
+
+report_dict = classification_report(y_test, test_predictions, target_names=['Real', 'AI'], output_dict=True)
+
+report_df = pd.DataFrame(report_dict)
+
+plot_df = report_df.iloc[:-1, :2].T  
+
+plot_df.plot(kind='bar', figsize=(8, 5), colormap='viridis', edgecolor='black')
+
+plt.title('Random Forest Performance Metrics', fontsize=14, fontweight='bold')
+plt.ylabel('Score (0.0 to 1.0)', fontsize=12)
+plt.xticks(rotation=0, fontsize=12)
+plt.ylim(0, 1.1)
+plt.legend(loc='lower right')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+plt.savefig('classification_report_chart.png', bbox_inches='tight')
+print("Chart saved successfully as 'classification_report_chart.png'!")
+ConfusionMatrixDisplay.from_predictions(y_test, test_predictions, display_labels=['Real (0)', 'AI (1)'], cmap='Blues')
+
+plt.title('Test Dataset Confusion Matrix')
+plt.savefig('confusion_matrix.png', bbox_inches='tight')
+print("Confusion Matrix saved successfully!")
+
+ConfusionMatrixDisplay.from_predictions(y_test, test_predictions, display_labels=['Real (0)', 'AI (1)'], cmap='Blues')
+
+plt.title('Test Dataset Confusion Matrix')
+plt.savefig('confusion_matrix.png', bbox_inches='tight')
+print("Confusion Matrix saved successfully!")
